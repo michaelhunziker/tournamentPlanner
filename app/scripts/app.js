5a3ed2c9a12f9241a40cb823f68690c1f8ad32ad
Michael Hunziker <michael.hunziker.1983@gmail.com>
2015-12-04 15:37:16 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tournamentPlannerApp
+ * @description
+ * # tournamentPlannerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tournamentPlannerApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tournamentPlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tournamentPlannerApp
+ */
+angular.module('tournamentPlannerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
